afe13c3463014454c54972eaa30a7a29502c2543
amurph37 <amurph.910@gmail.com>
2024-07-22 19:17:39 -0400

    JAVA Practice

---
+++ HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String friendName = "Tyler";
+        System.out.println("Hi " + friendName + ", how are you?");
+    }
+}
---
+++ Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I would like to visit");
+        System.out.println("multiple cities");
+        System.out.println("in IceLand");
+    }
+}
