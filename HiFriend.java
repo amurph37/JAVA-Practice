2ad9d9b95e2bf908510e596d807f8c513e693081
amurph37 <amurph.910@gmail.com>
2024-07-22 19:22:39 -0400

    small change

--- HiFriend.java
+++ HiFriend.java
@@ -1,6 +1,6 @@
 public class HiFriend {
     public static void main(String[] args) {
-        String friendName = "Tyler";
-        System.out.println("Hi " + friendName + ", how are you?");
+        String friendName = "Tyler Johnson";
+        System.out.println("Hi " + friendName + "!");
     }
 }
